a631fe727f46b1ced78c057f9e6754a7a06232c2
Bill Willits <bill.willits@getordermark.com>
2022-10-23 21:01:04 -0400

    test deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Brewhaus Dog Bones</h1>
+        <h1 className={styles.title}>Brewhaus Dog Bones!</h1>
       </main>
 
       <footer className={styles.footer}>
