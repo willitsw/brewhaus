c02684692f26df03b3e1687bd17048edee0f89e5
Bill Willits <bill.willits@getordermark.com>
2022-10-25 09:15:30 -0400

    slide show v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,37 @@ import type { NextPage } from 'next'
 import Head from "next/head";
 import Image from "next/image";
 import logo from "../images/logo.png";
+import Client, { Product } from "shopify-buy";
+import { useEffect, useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import blantons from "../images/bourbon-blantons.jpeg";
+import eagle from "../images/bourbon-eagle.jpeg";
+import knob from "../images/bourbon-knob.jpeg";
+import oldVanWinkle from "../images/bourbon-old-van-winkle.jpeg";
+import stag from "../images/bourbon-stag.jpeg";
+import taylor from "../images/bourbon-taylor.jpeg";
+import turkey from "../images/bourbon-turkey.jpeg";
+import vanWinkle from "../images/bourbon-van-winkle.jpeg";
+import weller from "../images/bourbon-weller.jpeg";
+import woodford from "../images/bourbon-woodford.jpeg";
+
+const client = Client.buildClient({
+  domain: "brewhaus-bourbon-raffle-test.myshopify.com",
+  storefrontAccessToken: "bb5a0f9a3f9a953c5bb98914b66fdc4a",
+});
 
 const Home: NextPage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const newProducts = await client.product.fetchAll();
+      setProducts(newProducts);
+    };
+    getProducts();
+  }, []);
+
   return (
     <div style={{ marginRight: "auto", marginLeft: "auto", maxWidth: 960 }}>
       <Head>
@@ -16,7 +45,28 @@ const Home: NextPage = () => {
       </header>
 
       <main>
-        <h1>Brewhaus Bourbon Raffle!</h1>
+        <Carousel
+          autoPlay
+          showStatus={false}
+          showArrows
+          infiniteLoop
+          showThumbs={false}
+        >
+          {[
+            blantons,
+            eagle,
+            knob,
+            oldVanWinkle,
+            stag,
+            taylor,
+            turkey,
+            vanWinkle,
+            weller,
+            woodford,
+          ].map((bourbon, key) => (
+            <Image key={key} src={bourbon} alt="Bourbon pics for auction" />
+          ))}
+        </Carousel>
       </main>
 
       <footer
