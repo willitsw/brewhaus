877c1627ba8e47cda04eb2c889db396084425fad
Bill Willits <bill.willits@getordermark.com>
2022-10-25 21:40:07 -0400

    simplify config

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig
