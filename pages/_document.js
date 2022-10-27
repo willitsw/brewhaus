b41a3eb8cb4cd10f1a16b4876992c7127800d42c
Bill Willits <bill.willits@getordermark.com>
2022-10-27 08:28:02 -0400

    remove antd styles

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito&family=Open+Sans:wght@600&display=swap"
           rel="stylesheet"
