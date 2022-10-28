a6917fee68709f0a8b7be36ed3c9a3ddf7d3e041
Bill Willits <bill.willits@getordermark.com>
2022-10-27 22:00:18 -0400

    lipsm

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,7 +63,7 @@ const Home: NextPage = () => {
         </Head>
         <Header
           style={{
-            minHeight: isTabletOrMobile ? 110 : 150,
+            minHeight: isTabletOrMobile ? 100 : 150,
             padding: 0,
           }}
         >
@@ -96,7 +96,7 @@ const Home: NextPage = () => {
                 textAlign: "right",
               }}
             >
-              Brewhaus Dog Bones Bourbon Raffle 2022!
+              Brewhaus Dog Bones Bourbon Raffle 2022
             </Typography.Title>
           </div>
         </Header>
@@ -145,6 +145,26 @@ const Home: NextPage = () => {
                 Buy Now
               </Button>
             </div>
+            <Divider />
+            <Typography.Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Typography.Paragraph>
+            <Divider />
+            <Typography.Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Typography.Paragraph>
           </div>
         </Content>
 
