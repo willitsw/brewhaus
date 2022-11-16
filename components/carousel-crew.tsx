c87484929967f7b29e3024f7acf05aa6f6457972
Bill Willits <bill.willits@getordermark.com>
2022-11-15 22:08:39 -0500

    fixes

--- components/carousel-crew.tsx
+++ components/carousel-crew.tsx
@@ -9,6 +9,7 @@ import crew3 from "../images/crew3.png";
 import crew4 from "../images/crew4.png";
 import crew5 from "../images/crew5.png";
 import crew6 from "../images/crew6.png";
+import crew7 from "../images/crew7.png";
 
 export const CarouselCrew = () => {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
@@ -22,11 +23,13 @@ export const CarouselCrew = () => {
   return (
     <div style={{ overflow: "hidden" }} ref={emblaRef}>
       <div style={{ display: "flex" }}>
-        {[crew1, crew2, crew3, crew4, crew5, crew6].map((bourbon, key) => (
-          <div key={key} style={{ flex: "0 0 100%" }}>
-            <Image src={bourbon} alt="The Brew Crew" />
-          </div>
-        ))}
+        {[crew1, crew2, crew7, crew3, crew4, crew5, crew6].map(
+          (bourbon, key) => (
+            <div key={key} style={{ flex: "0 0 100%" }}>
+              <Image src={bourbon} alt="The Brew Crew" />
+            </div>
+          )
+        )}
       </div>
     </div>
   );
